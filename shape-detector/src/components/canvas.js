ae98debfdbbc3a07c5f4d7f99392cb757937dfb2
DaScientist <37672507+DaScientist@users.noreply.github.com>
2020-05-26 16:18:01 +0530

    start commit

---
+++ shape-detector/src/components/canvas.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CustomCanvas(props) {
+  return (
+    <>
+      Hello {props.namem}, {props.named}, {props.nameb}, {props.namen}
+    </>
+  );
+}
+
+export default CustomCanvas;
